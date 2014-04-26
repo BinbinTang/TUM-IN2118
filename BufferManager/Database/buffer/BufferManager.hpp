8df1c6981e8b3c9e5fde3839a7bcf1e6701f01c3
cyk235 <ykchowab@stu.ust.hk>
2014-04-26 22:27:43 +0800

    Update BufferManager.hpp

--- BufferManager/Database/buffer/BufferManager.hpp
+++ BufferManager/Database/buffer/BufferManager.hpp
@@ -1,5 +1,9 @@
 class BufferManager{
     
 public:
-    BufferManager();
+    BufferManager(unsigned size);
+    BufferFrame& fixPage(uint64_t pageId, bool exclusive);
+    void unfixPage(BufferFrame& frame, bool isDirty);
+    void* getData();
+    ~BufferManager();
 };
